59a1ab4da4e778818482494ab6b3ebf219ffe687
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-05-27 10:20:22 +0300

    .

--- banking_project/accounts/urls.py
+++ banking_project/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+   # path('', views.index, name='index'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('encrypt/', views.encrypt_data, name='encrypt'),
