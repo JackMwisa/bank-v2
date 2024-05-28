bfeccb5a3ea4cd12b4b1a64bf6db0a2f5cc914b6
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-05-28 04:03:50 +0300

    done

--- accounts/models.py
+++ accounts/models.py
@@ -1,19 +1,17 @@
 from decimal import Decimal
-
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import (
-    MinValueValidator,
-    MaxValueValidator,
-)
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
-
 from .constants import GENDER_CHOICE
 from .managers import UserManager
-
 
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, null=False, blank=False)
+    public_key = models.TextField(null=True, blank=True)
+    encrypted_private_key = models.TextField(null=True, blank=True)
+    iv = models.TextField(null=True, blank=True)
+    symmetric_key = models.TextField(null=True, blank=True)
 
     objects = UserManager()
 
@@ -29,13 +27,9 @@ class User(AbstractUser):
             return self.account.balance
         return 0
 
-
 class BankAccountType(models.Model):
     name = models.CharField(max_length=128)
-    maximum_withdrawal_amount = models.DecimalField(
-        decimal_places=2,
-        max_digits=12
-    )
+    maximum_withdrawal_amount = models.DecimalField(decimal_places=2, max_digits=12)
     annual_interest_rate = models.DecimalField(
         validators=[MinValueValidator(0), MaxValueValidator(100)],
         decimal_places=2,
@@ -51,70 +45,32 @@ class BankAccountType(models.Model):
         return self.name
 
     def calculate_interest(self, principal):
-        """
-        Calculate interest for each account type.
-
-        This uses a basic interest calculation formula
-        """
         p = principal
         r = self.annual_interest_rate
         n = Decimal(self.interest_calculation_per_year)
-
-        # Basic Future Value formula to calculate interest
-        interest = (p * (1 + ((r/100) / n))) - p
-
+        interest = (p * (1 + ((r / 100) / n))) - p
         return round(interest, 2)
 
-
 class UserBankAccount(models.Model):
-    user = models.OneToOneField(
-        User,
-        related_name='account',
-        on_delete=models.CASCADE,
-    )
-    account_type = models.ForeignKey(
-        BankAccountType,
-        related_name='accounts',
-        on_delete=models.CASCADE
-    )
+    user = models.OneToOneField(User, related_name='account', on_delete=models.CASCADE)
+    account_type = models.ForeignKey(BankAccountType, related_name='accounts', on_delete=models.CASCADE)
     account_no = models.PositiveIntegerField(unique=True)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICE)
     birth_date = models.DateField(null=True, blank=True)
-    balance = models.DecimalField(
-        default=0,
-        max_digits=12,
-        decimal_places=2
-    )
-    interest_start_date = models.DateField(
-        null=True, blank=True,
-        help_text=(
-            'The month number that interest calculation will start from'
-        )
-    )
+    balance = models.DecimalField(default=0, max_digits=12, decimal_places=2)
+    interest_start_date = models.DateField(null=True, blank=True, help_text='The month number that interest calculation will start from')
     initial_deposit_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return str(self.account_no)
 
     def get_interest_calculation_months(self):
-        """
-        List of month numbers for which the interest will be calculated
-
-        returns [2, 4, 6, 8, 10, 12] for every 2 months interval
-        """
-        interval = int(
-            12 / self.account_type.interest_calculation_per_year
-        )
+        interval = int(12 / self.account_type.interest_calculation_per_year)
         start = self.interest_start_date.month
         return [i for i in range(start, 13, interval)]
 
-
 class UserAddress(models.Model):
-    user = models.OneToOneField(
-        User,
-        related_name='address',
-        on_delete=models.CASCADE,
-    )
+    user = models.OneToOneField(User, related_name='address', on_delete=models.CASCADE)
     street_address = models.CharField(max_length=512)
     city = models.CharField(max_length=256)
     postal_code = models.PositiveIntegerField()
--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,18 @@
 from django.contrib import messages
-from django.contrib.auth import get_user_model, login, logout, authenticate
+from django.contrib.auth import get_user_model, login, logout
 from django.contrib.auth.views import LoginView
 from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, RedirectView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
+from django.views.generic import TemplateView, RedirectView, View
+from django.http import JsonResponse
 from .forms import UserRegistrationForm, UserAddressForm
-
-from django.views.generic import View
-from .custom_crypto import CustomCrypto  # Import your custom encryption module
-
-
-
+from .digital_signature import sign_data, verify_signature
+from .encryption import encrypt_data, decrypt_data
+from .hash_function import calculate_hash
+from .key_management import generate_symmetric_key
+from Cryptodome.PublicKey import RSA
 
 User = get_user_model()
-
 
 class UserRegistrationView(TemplateView):
     model = User
@@ -24,9 +21,7 @@ class UserRegistrationView(TemplateView):
 
     def dispatch(self, request, *args, **kwargs):
         if self.request.user.is_authenticated:
-            return HttpResponseRedirect(
-                reverse_lazy('transactions:transaction_report')
-            )
+            return HttpResponseRedirect(reverse_lazy('transactions:transaction_report'))
         return super().dispatch(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
@@ -42,14 +37,9 @@ class UserRegistrationView(TemplateView):
             login(self.request, user)
             messages.success(
                 self.request,
-                (
-                    f'Thank You For Creating A Bank Account. '
-                    f'Your Account Number is {user.account.account_no}. '
-                )
+                f'Thank You For Creating A Bank Account. Your Account Number is {user.account.account_no}.'
             )
-            return HttpResponseRedirect(
-                reverse_lazy('transactions:deposit_money')
-            )
+            return HttpResponseRedirect(reverse_lazy('transactions:deposit_money'))
 
         return self.render_to_response(
             self.get_context_data(
@@ -66,11 +56,9 @@ class UserRegistrationView(TemplateView):
 
         return super().get_context_data(**kwargs)
 
-
 class UserLoginView(LoginView):
-    template_name='accounts/user_login.html'
+    template_name = 'accounts/user_login.html'
     redirect_authenticated_user = True
-
 
 class LogoutView(RedirectView):
     pattern_name = 'home'
@@ -79,3 +67,41 @@ class LogoutView(RedirectView):
         if self.request.user.is_authenticated:
             logout(self.request)
         return super().get_redirect_url(*args, **kwargs)
+
+# New views for cryptographic operations
+
+class SignMessageView(View):
+    def post(self, request, *args, **kwargs):
+        data = request.POST['data']
+        private_key = RSA.import_key(bytes.fromhex(request.user.encrypted_private_key))
+        signature = sign_data(data, private_key)
+        return JsonResponse({'signature': signature.hex()})
+
+class VerifyMessageView(View):
+    def post(self, request, *args, **kwargs):
+        data = request.POST['data']
+        signature = bytes.fromhex(request.POST['signature'])
+        public_key = RSA.import_key(request.user.public_key)
+        is_valid = verify_signature(data, signature, public_key)
+        return JsonResponse({'is_valid': is_valid})
+
+class EncryptMessageView(View):
+    def post(self, request, *args, **kwargs):
+        data = request.POST['data']
+        key = generate_symmetric_key()
+        ct_bytes, iv = encrypt_data(data, key)
+        return JsonResponse({'ciphertext': ct_bytes.hex(), 'iv': iv.hex(), 'key': key.hex()})
+
+class DecryptMessageView(View):
+    def post(self, request, *args, **kwargs):
+        ct_bytes = bytes.fromhex(request.POST['ciphertext'])
+        iv = bytes.fromhex(request.POST['iv'])
+        key = bytes.fromhex(request.POST['key'])
+        plaintext = decrypt_data(ct_bytes, iv, key)
+        return JsonResponse({'plaintext': plaintext})
+
+class HashMessageView(View):
+    def post(self, request, *args, **kwargs):
+        data = request.POST['data']
+        hash_value = calculate_hash(data)
+        return JsonResponse({'hash': hash_value})
